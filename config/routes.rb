c924e5fdcd3d35a71500d7e30af52d917536238c
Ignacio Moran <moran.ua@gmail.com>
2021-05-30 18:31:30 -0400

    Root changed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :concerts
   resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "concerts#index"
 end
